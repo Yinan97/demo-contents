d48d97029340b6f796353aee3eadf303cc5ce164
Yinan Liu <Yinan.Liu@marsladder.com.au>
2024-05-08 17:11:54 +0800

    read content given root id

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import dataclasses
 import json
 import sqlite3
 import uuid
+from pprint import pprint
 from uuid import uuid5
 
 
@@ -14,8 +15,31 @@ class ContentLineItem:
 
 @dataclasses.dataclass
 class Content:
+    id = None
     name: str
     sub_contents: list
+
+    def __init__(self, id, name, sub_contents):
+        self.id = id
+        self.name = name
+        self.sub_contents = sub_contents
+
+    @staticmethod
+    def from_entity(content_line_item: ContentLineItem):
+        if content_line_item.sub_content_ids is None:
+            return Content(
+                id=content_line_item.id,
+                name=content_line_item.name,
+                sub_contents=[]
+            )
+        else:
+            sub_content_line_items = list(map(get_content_line_item, content_line_item.sub_content_ids))
+            print(sub_content_line_items)
+            return Content(
+                id=content_line_item.id,
+                name=content_line_item.name,
+                sub_contents=list(map(Content.from_entity, sub_content_line_items))
+            )
 
 
 def save_content(content: Content):
@@ -57,56 +81,79 @@ def create_id(string, sub_content_ids=[]):
         return str(uuid5(uuid.NAMESPACE_URL, string + "|".join(sub_content_ids)))
 
 
+def get_content_line_item(content_root_id: str):
+    print(f"getting {content_root_id}")
+    with sqlite3.connect('contents.sqlite') as conn:
+        cursor = conn.cursor()
+        cursor.execute("select * from content_line_items where id = ?", (content_root_id,))
+        row = cursor.fetchone()
+        id, name, sub_content_ids_json = row
+        content_line_item = ContentLineItem(id, name, json.loads(sub_content_ids_json))
+        print(content_line_item)
+        return content_line_item
+
+
 if __name__ == '__main__':
-    contents_v1 = [
-        Content(
-            name="content_root",
-            sub_contents=[
-                Content(
-                    name="level 1",
-                    sub_contents=[
-                        Content(name="1.1", sub_contents=[]),
-                        Content(name="1.2", sub_contents=[])
-                    ]
-                ),
-                Content(
-                    name="level 2",
-                    sub_contents=[Content(name="2.1", sub_contents=[
-                        Content(name="2.1.1", sub_contents=[]),
-                    ])]
-                )
-            ]
-        )
-    ]
+    root_line_item = get_content_line_item("58d6778d-1222-5fc2-9877-2641c19c68a7")
+    print(root_line_item)
+    content_from_root = Content.from_entity(root_line_item)
+    pprint(content_from_root)
 
-    for content in contents_v1:
-        save_content(content)
+    root_line_item = get_content_line_item("dd65c30b-558a-55af-b873-66781f3ab9ed")
+    print(root_line_item)
+    content_from_root = Content.from_entity(root_line_item)
+    pprint(content_from_root)
 
-    print("--------------------")
-
-    contents_v2 = [
-        Content(
-            name="content_root",
-            sub_contents=[
-                Content(
-                    name="level 1",
-                    sub_contents=[
-                        Content(name="1.1", sub_contents=[]),
-                    ]
-                ),
-                Content(
-                    name="level 2",
-                    sub_contents=[Content(name="2.1", sub_contents=[
-                        Content(name="2.1.1", sub_contents=[]),
-                    ]), ]
-                )
-            ]
-        )
-    ]
-
-    for content in contents_v2:
-        save_content(content)
-
-    empty_content = [Content(name="content_root", sub_contents=[])]
-    for content in empty_content:
-        save_content(content)
+# if __name__ == '__main__':
+#     contents_v1 = [
+#         Content(
+#             name="content_root",
+#             sub_contents=[
+#                 Content(
+#                     name="level 1",
+#                     sub_contents=[
+#                         Content(name="1.1", sub_contents=[]),
+#                         Content(name="1.2", sub_contents=[])
+#                     ]
+#                 ),
+#                 Content(
+#                     name="level 2",
+#                     sub_contents=[Content(name="2.1", sub_contents=[
+#                         Content(name="2.1.1", sub_contents=[]),
+#                     ])]
+#                 )
+#             ]
+#         )
+#     ]
+# 
+#     for content in contents_v1:
+#         save_content(content)
+# 
+#     print("--------------------")
+# 
+#     contents_v2 = [
+#         Content(
+#             name="content_root",
+#             sub_contents=[
+#                 Content(
+#                     name="level 1",
+#                     sub_contents=[
+#                         Content(name="1.1", sub_contents=[]),
+#                     ]
+#                 ),
+#                 Content(
+#                     name="level 2",
+#                     sub_contents=[Content(name="2.1", sub_contents=[
+#                         Content(name="2.1.1", sub_contents=[]),
+#                     ]), ]
+#                 )
+#             ]
+#         )
+#     ]
+# 
+#     for content in contents_v2:
+#         save_content(content)
+# 
+#     empty_content = [Content(name="content_root", sub_contents=[])]
+#     for content in empty_content:
+#         save_content(content)
