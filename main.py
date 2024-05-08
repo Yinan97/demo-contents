743bd9a5e993eb004b0fe46f6ea93c567ca8b6a1
Yinan Liu <Yinan.Liu@marsladder.com.au>
2024-05-08 15:42:17 +0800

    fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import dataclasses
 import json
 import sqlite3
+import uuid
+from uuid import uuid5
 
 
 @dataclasses.dataclass
@@ -16,34 +18,58 @@ class Content:
     sub_contents: list
 
 
-def save(contents: list):
-    content: Content
-    for content in contents:
-        print(f"saving {content.name}")
+# def save(contents: list):
+#     content: Content
+#     if contents == []:
+#         return
+#     for content in contents:
+#         print(f"saving {content.name}")
+#
+#         if is_bottom_line_item(content):
+#             line_item = ContentLineItem(
+#                 id=create_id(content.name),
+#                 name=content.name,
+#                 sub_content_ids=[])
+#             id = save_line_item(line_item)
+#             return id
+#         else:
+#             line_item = ContentLineItem(
+#                 id=create_id(content.name),
+#                 name=content.name,
+#                 sub_content_ids=list(map(save, content.sub_contents))
+#             )
+#             id = save_line_item(line_item)
+#             return id
 
-        if is_bottom_line_item(content):
-            line_item = ContentLineItem(
-                id=create_id(content.name),
-                name=content.name,
-                sub_content_ids=[])
-            id = save_line_item(line_item)
-            return id
-        else:
-            line_item = ContentLineItem(
-                id=create_id(content.name),
-                name=content.name,
-                sub_content_ids=list(map(save, content.sub_contents))
-            )
-            id = save_line_item(line_item)
-            return id
+
+def save(content: Content):
+    if content == []:
+        return
+    if is_bottom_line_item(content):
+        line_item = ContentLineItem(
+            id=create_id(content.name),
+            name=content.name,
+            sub_content_ids=[])
+        id = save_line_item(line_item)
+        return id
+    else:
+        sub_content_ids=list(map(save, content.sub_contents))
+        line_item = ContentLineItem(
+            id=create_id(content.name),
+            name=content.name,
+            sub_content_ids=sub_content_ids
+        )
+        id = save_line_item(line_item)
+        return id
 
 
 def save_line_item(line_item: ContentLineItem):
     conn = sqlite3.connect('contents.sqlite')
     cursor = conn.cursor()
-    id = line_item.id
+    id = str(line_item.id)
     sub_ids = json.dumps(line_item.sub_content_ids)
     name = line_item.name
+    print(f"saving {line_item}")
     cursor.execute("INSERT INTO content_line_items(id, name, subcontent_ids) VALUES (?, ?, ?)",
                    (id, name, sub_ids))
     conn.commit()
@@ -55,18 +81,11 @@ def is_bottom_line_item(content: Content):
 
 
 def create_id(string):
-    return str(hash(string))
+    return uuid5(uuid.NAMESPACE_URL, string)
 
 
 if __name__ == '__main__':
-    # line_item = ContentLineItem(
-    #     id="abcd",
-    #     name="test 1",
-    #     sub_content_ids=["abc", "def"]
-    # )
-    # save_line_item(line_item)
-
-    contents= [
+    contents = [
         Content(
             name="level 1",
             sub_contents=[Content(name="1.1", sub_contents=[]),
@@ -80,4 +99,7 @@ if __name__ == '__main__':
         )
     ]
 
-    save(contents)
+    for content in contents:
+        save(content)
+
+    print(create_id("abc"))
